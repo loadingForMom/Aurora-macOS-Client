51d7cd19f792b1b3a61dbe80a7c98774a60fe0e4
loadingForMom <155063496+loadingForMom@users.noreply.github.com>
2026-01-07 02:00:45 +0300

    Fix parsed JSON cache visibility

--- Core/TDLib/TelegramStore.swift
+++ Core/TDLib/TelegramStore.swift
@@ -59,8 +59,8 @@ final class TelegramStore: ObservableObject {
 
     // MARK: - JSON parsing cache
 
-    private var lastParsedUpdate: String?
-    private var lastParsedObject: [String: Any]?
+    fileprivate var lastParsedUpdate: String?
+    fileprivate var lastParsedObject: [String: Any]?
 
     // MARK: - Thumbnail cache
 
