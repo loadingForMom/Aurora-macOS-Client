b4e60f5354fde9344d3abcf4e924f9bc2a252187
loadingForMom <sashavasilev56753@icloud.com>
2026-02-05 18:34:04 +0300

    Update AuroraApp.swift

--- App/AuroraApp.swift
+++ App/AuroraApp.swift
@@ -15,7 +15,7 @@ struct AuroraApp: App {
         WindowGroup {
             ContentView(store: store)
                 .environmentObject(store)
-                .bitmapLiveResize()
+                
         }
         .commands {
             InspectorCommands()
